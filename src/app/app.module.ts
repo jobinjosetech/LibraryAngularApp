65a2bfaf5972a08b40df6625fdc452f65ce4c39b
jobin <jobinjose.in@gmail.com>
2022-11-24 23:14:37 +0530

    user login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { UserRegisterationComponent } from './user-registeration/user-registeration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    UserRegisterationComponent
+    UserRegisterationComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
