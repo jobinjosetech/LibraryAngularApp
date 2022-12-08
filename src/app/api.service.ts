9fd71e0dce69382a09ccd4d7bd9099fa300f7298
jobin <jobinjose.in@gmail.com>
2022-12-09 02:26:39 +0530

    api integration

---
+++ src/app/api.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  userSignup = (data:any)=>{
+    return this.http.post("http://localhost:8080/userRegistration",data)
+  }
+
+  userLogin = (data:any)=>{
+    return this.http.post("http://localhost:8080/userLogin",data)
+  }
+
+  addBook = (data:any)=>{
+    console.log(data)
+    return this.http.post("http://localhost:8080/addBook",data)
+  }
+
+  fetchBook = ()=>{
+    return this.http.get("http://localhost:8080/viewBook")
+  }
+
+  searchBook = (data:any)=>{
+    return this.http.post("http://localhost:8080/searchBook",data)
+  }
+
+  deleteBook = (data:any) =>{
+    return this.http.post("http://localhost:8080/deleteBook",data)
+  }
+
+  updateBook = (data:any)=>{
+    return this.http.post("http://localhost:8080/editBook",data)
+  }
+
+  getBookById = (data:any)=>{
+    return this.http.post("http://localhost:8080/getBookById",data)
+  }
+}
